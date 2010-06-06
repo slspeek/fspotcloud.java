5f1b1ea4a3a8189c9ad6033dcf0c616549bd0f93
Steven L. Speek <slspeek@gmail.com>
2010-06-06 20:40:17 +0200

    Inital version of the Tag class.

---
+++ src/fspotcloud/server/tag/Tag.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package fspotcloud.server.tag;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+
+/**
+ * @author rocco
+ *
+ */
+public class Tag {
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key key;
+	
+	 @Persistent
+	 private String name;
+
+	 @Persistent
+	 private String description;
+
+	/**
+	 * @param key the key to set
+	 */
+	public void setKey(Key key) {
+		this.key = key;
+	}
+
+	/**
+	 * @return the key
+	 */
+	public Key getKey() {
+		return key;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+}
