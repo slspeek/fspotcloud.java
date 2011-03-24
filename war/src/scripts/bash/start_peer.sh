8297c721c459599ad1e9eebaca433c03be7d537e
Steven L. Speek <slspeek@gmail.com>
2011-03-24 23:53:21 +0100

    Selenium2 can run tests against the local deployment including the peer.

---
+++ war/src/scripts/bash/start_peer.sh
@@ -0,0 +1,1 @@
+java -Dendpoint=localhost:8080 -jar ../peer/target/peer-0.39-jar-with-dependencies.jar &
--- war/src/test/java/fspotcloud/test/AdminITest.java
+++ war/src/test/java/fspotcloud/test/AdminITest.java
@@ -35,4 +35,22 @@ public class AdminITest extends TestCase {
 		selenium.click("action");
 		selenium.waitForPageToLoad("30000");
 	}
+	
+	public void testImportTags() throws Exception {
+		selenium.open("http://localhost:8080/Admin.html");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("isAdmin");
+		selenium.click("action");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("//tr[3]/td[2]/button");
+		try {
+			Thread.sleep(15000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		//selenium.wait(10000);
+		selenium.open("http://localhost:8080/");
+		selenium.waitForPageToLoad("30000");
+		
+	}
 }
