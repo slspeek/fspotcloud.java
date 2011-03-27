70bf34ab1c542006d8700e3f2a5b8ebdf58ee939
Steven L. Speek <slspeek@gmail.com>
2011-03-27 23:22:37 +0200

    Integration test tests image transport from peer to server.

--- war/src/test/java/fspotcloud/test/AdminITest.java
+++ war/src/test/java/fspotcloud/test/AdminITest.java
@@ -25,7 +25,7 @@ public class AdminITest extends TestCase {
 	}
 
 	public void testAdminI() throws Exception {
-		selenium.open("http://localhost:8080/");
+		selenium.open("/Admin.html");
 		selenium.waitForPageToLoad("30000");
 		selenium.click("isAdmin");
 		selenium.click("action");
@@ -41,7 +41,7 @@ public class AdminITest extends TestCase {
 		selenium.click("//tr[3]/td[2]/button");
 		selenium.waitForPageToLoad("30000");
 		try {
-			Thread.sleep(5000);
+			Thread.sleep(2000);
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
@@ -51,10 +51,35 @@ public class AdminITest extends TestCase {
 			Thread.sleep(5000);
 		} catch (Exception e) {
 			// TODO: handle exception
-		}selenium.click("//tr[4]/td[2]/button");
+		}
+		selenium.open("/Admin.html");
+		selenium.waitForPageToLoad("30000");
+		try {
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+
+		selenium.open("/Admin.html");
+		selenium.waitForPageToLoad("30000");
+		try {
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		selenium.click("//tr[4]/td[2]/button");
 		selenium.waitForPageToLoad("30000");
 		try {
 			Thread.sleep(15000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		selenium.open("/Admin.html");
+		selenium.waitForPageToLoad("30000");
+		try {
+			Thread.sleep(3000);
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
@@ -73,19 +98,59 @@ public class AdminITest extends TestCase {
 
 		selenium.waitForPageToLoad("30000");
 		selenium.click("link=Import tag 3");
-		selenium.open("/TestHelper.html");
+		selenium.waitForPageToLoad("30000");
+		selenium.open("/Admin.html");
 		selenium.waitForPageToLoad("30000");
 		try {
-			Thread.sleep(30000);
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		try {
+			Thread.sleep(15000);
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 		selenium.open("/");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("//td[3]/div");
 		try {
-			Thread.sleep(30000);
+			Thread.sleep(900);
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
+		selenium.click("//td[3]/div");
+		try {
+			Thread.sleep(900);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		selenium.click("//div[1]/div[2]/div[1]/div/div[2]");
 		selenium.waitForPageToLoad("30000");
+
+		selenium.click("//td[3]/div");
+		selenium.waitForPageToLoad("30000");
+
+		try {
+			Thread.sleep(900);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		selenium.click("//td[5]/div");
+		try {
+			Thread.sleep(100);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		assertTrue(selenium.isTextPresent("4 seconds."));
+		selenium.open("/#ImageViewingPlace:1:27");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("//td[1]/div/table/tbody/tr/td[1]/div");
+		selenium.waitForPageToLoad("30000");
+		try {
+			Thread.sleep(900);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 }
