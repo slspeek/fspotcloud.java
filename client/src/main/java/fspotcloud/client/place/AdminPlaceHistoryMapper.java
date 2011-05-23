5e16bf250b022edacf1b0b435d229f7dae000ed0
Steven L. Speek <slspeek@gmail.com>
2011-05-23 23:14:01 +0200

    Added sanselan dependency

--- client/src/main/java/fspotcloud/client/place/AdminPlaceHistoryMapper.java
+++ client/src/main/java/fspotcloud/client/place/AdminPlaceHistoryMapper.java
@@ -3,8 +3,6 @@ package fspotcloud.client.place;
 import com.google.gwt.place.shared.PlaceHistoryMapper;
 import com.google.gwt.place.shared.WithTokenizers;
 
-import fspotcloud.client.place.TagViewingPlace.Tokenizer;
-
 @WithTokenizers({TagPlace.Tokenizer.class})
 public interface AdminPlaceHistoryMapper extends PlaceHistoryMapper {
 
