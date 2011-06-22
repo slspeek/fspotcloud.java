a91f662eecf2431bfa7dd0aaccd441ca121bad30
Steven L. Speek <slspeek@gmail.com>
2011-06-22 21:49:46 +0200

    T toggles between one image mode and tabular view.

--- client/src/test/java/fspotcloud/client/main/PlaceSwapperTest.java
+++ client/src/test/java/fspotcloud/client/main/PlaceSwapperTest.java
@@ -1,28 +1,39 @@
 package fspotcloud.client.main;
 
 import junit.framework.TestCase;
-import fspotcloud.client.place.ImageViewingPlace;
-import fspotcloud.client.place.TagViewingPlace;
 
 import com.google.gwt.place.shared.Place;
+
+import fspotcloud.client.place.BasePlace;
+import fspotcloud.client.place.ImageViewingPlace;
+import fspotcloud.client.place.TagViewingPlace;
 
 public class PlaceSwapperTest extends TestCase {
 
 	PlaceSwapper swapper = new PlaceSwapper();
 	
 	public void testSimpleSwap() {
-		Place imageViewingPlace = new ImageViewingPlace("1", "10");
-		Place newPlace = swapper.swap(imageViewingPlace);
+		BasePlace imageViewingPlace = new ImageViewingPlace("1", "10");
+		BasePlace newPlace = swapper.toggleTreeViewVisible(imageViewingPlace);
 		assertEquals(TagViewingPlace.class, newPlace.getClass());
-		assertEquals("1", ((TagViewingPlace)newPlace).getTagId());
-		assertEquals("10", ((TagViewingPlace)newPlace).getPhotoId());
+		assertEquals("1", newPlace.getTagId());
+		assertEquals("10", newPlace.getPhotoId());
 	}
 	
 	public void testReverse() {
-		Place tagViewingPlace = new TagViewingPlace("1", "10");
-		Place newPlace = swapper.swap(tagViewingPlace);
+		BasePlace tagViewingPlace = new TagViewingPlace("1", "10");
+		Place newPlace = swapper.toggleTreeViewVisible(tagViewingPlace);
 		assertEquals(ImageViewingPlace.class, newPlace.getClass());
 		assertEquals("1", ((ImageViewingPlace)newPlace).getTagId());
 		assertEquals("10", ((ImageViewingPlace)newPlace).getPhotoId());
+	}
+	
+	public void testToggleRasterView() {
+		BasePlace tagViewingPlace = new ImageViewingPlace("1", "10", 2, 1);
+		ImageViewingPlace newPlace = (ImageViewingPlace) swapper.toggleRasterView(tagViewingPlace);
+		assertEquals("1", ((ImageViewingPlace)newPlace).getTagId());
+		assertEquals("10", ((ImageViewingPlace)newPlace).getPhotoId());
+		assertEquals(1, newPlace.getColumnCount());
+		assertEquals(1, newPlace.getRowCount());
 	}
 }
