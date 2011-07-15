ced105935acdf33f96b128783d2b069ccc611fac
Steven L. Speek <slspeek@gmail.com>
2011-07-15 02:14:26 +0200

    Partial fix, builds again.

--- client/src/main/java/fspotcloud/client/view/action/KeyDispatcherProvider.java
+++ client/src/main/java/fspotcloud/client/view/action/KeyDispatcherProvider.java
@@ -1,5 +1,7 @@
 package fspotcloud.client.view.action;
 
+import java.util.logging.Logger;
+
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
@@ -8,19 +10,26 @@ import fspotcloud.client.view.action.api.UserAction;
 
 public class KeyDispatcherProvider implements Provider<KeyDispatcher> {
 
+	private static final Logger log = Logger
+	.getLogger(KeyDispatcherProvider.class.getName());
 	final private KeyDispatcher keyDispatcher = new KeyDispatcher();
 	final private AllUserActions actions;
 	
 	@Inject
 	public KeyDispatcherProvider(AllUserActions actions) {
 		this.actions = actions;
+		log.info("created");
 		registerShortcuts();
 	}
 
 	private void registerShortcuts() {
 		((AllShortcuts)actions).init();
 		for (UserAction action: actions.allActions()) {
-			keyDispatcher.register(action);
+			if (action != null) {
+				keyDispatcher.register(action);
+			} else {
+				log.warning("action was null");
+			}
 		}
 	}
 	@Override
