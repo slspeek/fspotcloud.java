acb20d019353476f6b79482a376e7883eb23a430
Steven L. Speek <slspeek@gmail.com>
2011-08-17 21:31:03 +0200

    Fix.

--- client/src/main/java/fspotcloud/client/view/action/api/UserAction.java
+++ client/src/main/java/fspotcloud/client/view/action/api/UserAction.java
@@ -1,13 +1,13 @@
 package fspotcloud.client.view.action.api;
 
+import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.inject.Provider;
-import com.google.web.bindery.event.shared.Event;
 
 public interface UserAction extends Runnable, ActionDef {
 	
 	ImageResource getIcon();
 	
-	Provider<? extends Event> getEventProvider();
+	Provider<? extends GwtEvent> getEventProvider();
 	
 }
