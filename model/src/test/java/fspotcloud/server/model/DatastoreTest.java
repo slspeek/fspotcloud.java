c7606db378ad5193516d13f210caccc9754aaa99
Steven L. Speek <slspeek@gmail.com>
2011-10-06 23:29:59 +0200

    Fixed the build.

---
+++ model/src/test/java/fspotcloud/server/model/DatastoreTest.java
@@ -0,0 +1,25 @@
+package fspotcloud.server.model;
+
+import junit.framework.TestCase;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+
+public abstract class DatastoreTest extends TestCase {
+
+	private final LocalServiceTestHelper helper =
+        new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+
+    public void setUp() {
+        helper.setUp();
+    }
+
+    public void tearDown() {
+        helper.tearDown();
+    }
+
+	public DatastoreTest() {
+	}
+
+
+}
