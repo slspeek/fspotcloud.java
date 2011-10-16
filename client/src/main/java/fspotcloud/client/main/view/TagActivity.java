65b85c831bccb3ce4338ac2678397d059530f94c
Steven L. Speek <slspeek@gmail.com>
2011-10-16 20:52:00 +0200

    Attempt to fix a repaint bug in image raster.

--- client/src/main/java/fspotcloud/client/main/view/TagActivity.java
+++ client/src/main/java/fspotcloud/client/main/view/TagActivity.java
@@ -3,9 +3,12 @@ package fspotcloud.client.main.view;
 import java.util.logging.Logger;
 
 import com.google.gwt.activity.shared.AbstractActivity;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
 
+import fspotcloud.client.main.view.api.ImageRasterView;
 import fspotcloud.client.main.view.api.TagView;
 
 public class TagActivity extends AbstractActivity implements
@@ -16,13 +19,22 @@ public class TagActivity extends AbstractActivity implements
 			.getName());
 
 	final private TagView tagView;
-
-	public TagActivity(TagView tagView) {
+	final private ImageRasterView.ImageRasterPresenter imageRasterPresenter;
+	
+	public TagActivity(TagView tagView, ImageRasterView.ImageRasterPresenter imageRasterPresenter) {
 		this.tagView = tagView;
+		this.imageRasterPresenter = imageRasterPresenter;
 	}
 
 	@Override
 	public void start(AcceptsOneWidget containerWidget, EventBus eventBus) {
 		containerWidget.setWidget(tagView);
+		Scheduler scheduler = Scheduler.get();
+		scheduler.scheduleDeferred(new ScheduledCommand() {
+			@Override
+			public void execute() {
+				imageRasterPresenter.init();
+			}
+		});
 	}
 }
