5034dd423fcb84ccab77ba899144787bd6698078
Steven L. Speek <slspeek@gmail.com>
2011-11-29 01:56:11 +0100

    Finally fixed the build.

--- server/src/main/java/fspotcloud/server/control/task/TaskActionsModule.java
+++ server/src/main/java/fspotcloud/server/control/task/TaskActionsModule.java
@@ -1,9 +1,11 @@
 package fspotcloud.server.control.task;
 
 import net.customware.gwt.dispatch.server.guice.ActionHandlerModule;
+import fspotcloud.server.control.task.actions.intern.DeletePhotos;
 import fspotcloud.server.control.task.actions.intern.DeleteTags;
 import fspotcloud.server.control.task.actions.intern.PhotoImportScheduleAction;
 import fspotcloud.server.control.task.actions.intern.TagImportAction;
+import fspotcloud.server.control.task.photodelete.DeletePhotosHandler;
 import fspotcloud.server.control.task.photoimport.PhotoImportScheduleHandler;
 import fspotcloud.server.control.task.tagdelete.DeleteTagsHandler;
 import fspotcloud.server.control.task.tagimport.TagImportHandler;
@@ -15,5 +17,6 @@ public class TaskActionsModule extends ActionHandlerModule {
 		bindHandler(PhotoImportScheduleAction.class, PhotoImportScheduleHandler.class);
 		bindHandler(TagImportAction.class, TagImportHandler.class);
 		bindHandler(DeleteTags.class, DeleteTagsHandler.class);
+		bindHandler(DeletePhotos.class, DeletePhotosHandler.class);
 	}
 }
