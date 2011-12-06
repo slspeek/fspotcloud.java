54b74ed4df5d36cffb67f6ae23ced4f284a5b4ff
Steven L. Speek <slspeek@gmail.com>
2011-12-06 23:05:19 +0100

    Used Mockito annotations.

---
+++ server/src/test/java/fspotcloud/server/control/task/tagdelete/DeleteTagsHandlerTest.java
@@ -0,0 +1,53 @@
+package fspotcloud.server.control.task.tagdelete;
+
+import junit.framework.TestResult;
+
+import net.customware.gwt.dispatch.shared.DispatchException;
+
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+
+import fspotcloud.server.control.task.actions.intern.DeleteTags;
+import fspotcloud.server.model.api.Tags;
+import fspotcloud.taskqueuedispatch.TaskQueueDispatch;
+import fspotcloud.test.MockitoTestCase;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
+
+public class DeleteTagsHandlerTest extends MockitoTestCase {
+
+	DeleteTagsHandler target;
+	
+	@Mock
+	TaskQueueDispatch dispatchAsync;
+	@Mock
+	Tags tagManager;
+	@Captor
+	ArgumentCaptor<DeleteTags> newAction;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		target = new DeleteTagsHandler(dispatchAsync, tagManager);
+		System.out.println(tagManager);
+	}
+
+	public void testRecursionStop() throws DispatchException {
+		when(tagManager.deleteAll()).thenReturn(true);
+		target.execute(new DeleteTags(), null);
+		verifyNoMoreInteractions(dispatchAsync);
+	}
+	
+	public void testRecursion() throws DispatchException {
+		when(tagManager.deleteAll()).thenReturn(false);
+		target.execute(new DeleteTags(), null);
+		verify(dispatchAsync).execute(newAction.capture());
+		assertNotNull(newAction.getValue());
+	}
+	
+
+	
+	
+}
