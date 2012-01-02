93c7c5b3731e6b449c0f7e2f8bc79e8025d6890a
Steven L. Speek <slspeek@gmail.com>
2012-01-02 22:34:13 +0100

    Moved taskqueue-dispatch and bot-dispatch to separate projects on http://code.google.com/p/taskqueuedispatch http://code.google.com/p/botdispatch resp.

--- bot-dispatch/bot/src/main/java/fspotcloud/botdispatch/bot/CommandWorkerFactory.java
+++
@@ -1,7 +0,0 @@
-package fspotcloud.botdispatch.bot;
-
-import net.customware.gwt.dispatch.shared.Action;
-
-public interface CommandWorkerFactory {
-	CommandWorker get(Action<?> action);
-}
--- taskqueue-dispatch/dispatch/src/main/java/fspotcloud/taskqueuedispatch/SerializableAsyncCallback.java
+++
@@ -1,9 +0,0 @@
-package fspotcloud.taskqueuedispatch;
-
-import java.io.Serializable;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface SerializableAsyncCallback<T> extends AsyncCallback<T>, Serializable{
-	
-}
