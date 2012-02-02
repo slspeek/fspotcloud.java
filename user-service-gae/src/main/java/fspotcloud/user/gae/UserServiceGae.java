220e4c07cb8be6f1bdb67a79522af50f43ce8a9d
Steven L. Speek <slspeek@gmail.com>
2012-02-02 21:15:22 +0100

    Logon/off starts to work

--- user-service-gae/src/main/java/fspotcloud/user/gae/UserServiceGae.java
+++ user-service-gae/src/main/java/fspotcloud/user/gae/UserServiceGae.java
@@ -1,18 +1,33 @@
 package fspotcloud.user.gae;
 
+import com.google.inject.Provider;
 import javax.inject.Inject;
-
+import javax.servlet.http.HttpServletRequest;
 
 public class UserServiceGae implements fspotcloud.user.UserService {
 
-    @Inject com.google.appengine.api.users.UserService delegate;
+    @Inject
+    com.google.appengine.api.users.UserService delegate;
+    @Inject
+    Provider<HttpServletRequest> requestProvider;
+
+    private String toAbsoluteURL(String url) {
+        HttpServletRequest request = requestProvider.get();
+        String result = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort()
+                + request.getContextPath() + "/" + url;
+        return result;
+
+    }
+
     @Override
     public String createLoginURL(String string) {
+        string = toAbsoluteURL(string);
         return delegate.createLoginURL(string);
     }
 
     @Override
     public String createLogoutURL(String string) {
+        string = toAbsoluteURL(string);
         return delegate.createLogoutURL(string);
     }
 
@@ -34,5 +49,4 @@ public class UserServiceGae implements fspotcloud.user.UserService {
     public boolean isUserAdmin() {
         return isUserLoggedIn() && delegate.isUserLoggedIn();
     }
-
 }
