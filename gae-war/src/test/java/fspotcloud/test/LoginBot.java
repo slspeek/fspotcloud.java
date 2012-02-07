4278fa7484968e1a9f32be08674707064dba0019
Steven L. Speek <slspeek@gmail.com>
2012-02-07 17:29:07 +0100

    Redirect disabled as it broke the selenium tests

--- gae-war/src/test/java/fspotcloud/test/LoginBot.java
+++ gae-war/src/test/java/fspotcloud/test/LoginBot.java
@@ -5,16 +5,18 @@ import javax.inject.Inject;
 import com.thoughtworks.selenium.Selenium;
 
 public class LoginBot {
-	
-	@Inject 
-	Selenium selenium;
-	
-	public void login() throws Exception {
-		selenium.open("/Dashboard.html");
-		selenium.waitForPageToLoad("30000");
-		selenium.click("isAdmin");
-		selenium.click("action");
-		selenium.waitForPageToLoad("30000");
-	}
 
+    @Inject
+    Selenium selenium;
+
+    public void login() throws Exception {
+        selenium.open("/");
+        selenium.waitForPageToLoad("30000");
+        selenium.click("gwt-debug-login");
+        selenium.waitForPageToLoad("30000");
+        selenium.click("isAdmin");
+        selenium.click("action");
+        selenium.waitForPageToLoad("30000");
+        selenium.open("/Dashboard.html");
+    }
 }
