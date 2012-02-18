c2f5f6558c3cd8297a26060847bdb7e0bdc1bc21
Steven L. Speek <slspeek@gmail.com>
2012-02-18 12:41:15 +0100

    peer test sets system properties to null

--- peer/src/test/java/fspotcloud/peer/db/DataTest.java
+++ peer/src/test/java/fspotcloud/peer/db/DataTest.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package fspotcloud.peer.db;
 
@@ -14,63 +14,64 @@ import fspotcloud.shared.peer.rpc.actions.PhotoData;
 import fspotcloud.shared.peer.rpc.actions.TagData;
 
 public class DataTest extends TestCase {
-	final static private Logger log = Logger
-			.getLogger(DataTest.class.getName());
-	private Data data;
 
-	protected void setUp() throws Exception {
-		super.setUp();
-		URL testDatabase = ClassLoader.getSystemResource("photos.db");
-		String path = testDatabase.getPath();
-		data = new Data("jdbc:sqlite:" +path);
-	}
+    final static private Logger log = Logger.getLogger(DataTest.class.getName());
+    private Data data;
 
-	protected void tearDown() throws Exception {
-		super.tearDown();
-		data = null;
-	}
+    protected void setUp() throws Exception {
+        super.setUp();
+        URL testDatabase = ClassLoader.getSystemResource("photos.db");
+        System.setProperty("photo.dir.original", null);
+        System.setProperty("photo.dir.override", null);
+        String path = testDatabase.getPath();
+        data = new Data("jdbc:sqlite:" + path);
+    }
 
-	public void testGetPhotoCount() throws SQLException {
-		int count = data.getCount("photos");
-		assertEquals(28, count);
-	}
-	
-	public void testGetTagCount() throws SQLException {
-		int count = data.getCount("tags");
-		assertEquals(5, count);
-	}
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        data = null;
+    }
 
-	public final void testGetTagList2() throws SQLException {
-		List<TagData> result = data.getTagData(0, 1);
-		assertEquals(1, result.size());
-	}
+    public void testGetPhotoCount() throws SQLException {
+        int count = data.getCount("photos");
+        assertEquals(28, count);
+    }
 
-	public final void testGetTagListOverItsSize() throws SQLException {
-		List<TagData> result = data.getTagData(8,2);
-		assertEquals(0, result.size());
-	}
+    public void testGetTagCount() throws SQLException {
+        int count = data.getCount("tags");
+        assertEquals(5, count);
+    }
 
-		public void testGetPhotoKeysForTag() throws Exception {
-		List<String> result = data.getPhotoKeysInTag("5");
-		assertEquals("3", result.get(0));
-	}
+    public final void testGetTagList2() throws SQLException {
+        List<TagData> result = data.getTagData(0, 1);
+        assertEquals(1, result.size());
+    }
 
-	public final void testGetImageURL() throws MalformedURLException,
-			SQLException {
-		String url = data.getImageURL("20");
-		assertEquals("file:///home/steven/Photos/2010/06/22/img_0859-1.jpg", String.valueOf(url));
-		
-	}
-	
-	
-	public void testIsPhotoInTag() throws Exception {
-		boolean yep = data.isPhotoInTag("5", "3");
-		assertTrue(yep);
-		boolean nope = data.isPhotoInTag("5", "4");
-		assertFalse(nope);
-	}
-	
-	public void testPhotoVersion() throws Exception {
-		assertEquals(1, data.getPhotoDefaultVersion("3"));
-	}
+    public final void testGetTagListOverItsSize() throws SQLException {
+        List<TagData> result = data.getTagData(8, 2);
+        assertEquals(0, result.size());
+    }
+
+    public void testGetPhotoKeysForTag() throws Exception {
+        List<String> result = data.getPhotoKeysInTag("5");
+        assertEquals("3", result.get(0));
+    }
+
+    public final void testGetImageURL() throws MalformedURLException,
+            SQLException {
+        String url = data.getImageURL("20");
+        assertEquals("file:///home/steven/Photos/2010/06/22/img_0859-1.jpg", String.valueOf(url));
+
+    }
+
+    public void testIsPhotoInTag() throws Exception {
+        boolean yep = data.isPhotoInTag("5", "3");
+        assertTrue(yep);
+        boolean nope = data.isPhotoInTag("5", "4");
+        assertFalse(nope);
+    }
+
+    public void testPhotoVersion() throws Exception {
+        assertEquals(1, data.getPhotoDefaultVersion("3"));
+    }
 }
