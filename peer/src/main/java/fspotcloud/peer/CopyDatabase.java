5eeaacdccbc23d16d6d3276fbe534d67f72ac4e9
Steven L. Speek <slspeek@gmail.com>
2012-03-02 17:33:46 +0100

    Injected logger in CopyDatabase, for buildserver bug.

--- peer/src/main/java/fspotcloud/peer/CopyDatabase.java
+++ peer/src/main/java/fspotcloud/peer/CopyDatabase.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.logging.Logger;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -13,46 +14,45 @@ import javax.inject.Provider;
 
 public class CopyDatabase implements Provider<String> {
 
-	@Inject
-	@Named("DatabasePath")
-	private String srcPath;
+    @Inject
+    Logger log;
+    @Inject
+    @Named("DatabasePath")
+    private String srcPath;
+    @Inject
+    @Named("WorkDir")
+    private String pwd;
 
-	@Inject
-	@Named("WorkDir")
-	private String pwd;
+    public String copyDatabase() throws IOException {
+        log.info("src: " + srcPath);
+        File srcFile = new File(srcPath);
+        File targetDir = new File(pwd + "/runtime");
+        if (!targetDir.exists()) {
+            targetDir.mkdir();
+        }
+        File targetFile = new File(targetDir.getAbsolutePath() + "/copy.db");
+        InputStream in = new FileInputStream(srcFile);
+        OutputStream out = new FileOutputStream(targetFile);
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
+        String url = "jdbc:sqlite:" + targetFile.getAbsolutePath();
+        log.info("url: " + url);
+        return url;
+    }
 
-
-	public String copyDatabase() throws IOException {
-            System.out.println("src: " +srcPath);
-		File srcFile = new File(srcPath);
-		File targetDir = new File(pwd + "/runtime");
-		if (!targetDir.exists()) {
-			targetDir.mkdir();
-		}
-		File targetFile = new File(targetDir.getAbsolutePath() + "/copy.db");
-		InputStream in = new FileInputStream(srcFile);
-		OutputStream out = new FileOutputStream(targetFile);
-		byte[] buf = new byte[1024];
-		int len;
-		while ((len = in.read(buf)) > 0) {
-			out.write(buf, 0, len);
-		}
-		in.close();
-		out.close();
-                String url = "jdbc:sqlite:" + targetFile.getAbsolutePath();
-                System.out.println("url: " + url);
-		return url;
-	}
-
-	@Override
-	public String get() {
-		String destPath = null;
-		try {
-			destPath = copyDatabase();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return destPath;
-	}
-
+    @Override
+    public String get() {
+        String destPath = null;
+        try {
+            destPath = copyDatabase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return destPath;
+    }
 }
