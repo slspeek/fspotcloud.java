275c08cadfc7ec17eff0571377633b7a3a2c80d6
Steven L. Speek <slspeek@gmail.com>
2012-04-02 21:14:43 +0200

    gradle builds gae-war

---
+++ gae-war/peer_run.sh
@@ -0,0 +1,10 @@
+VERSION=$1
+BOT_SECRET=$2
+java -cp "../peer/build/libs/peer-$VERSION.jar" \
+      -Ddb="../peer/src/test/resources/photos.db" \
+      -Dendpoint="localhost:8080" \
+      -Dbot.secret="${BOT_SECRET}" \
+      -Dpause=2 \
+      -Dphoto.dir.original="file:///home/steven/Photos" \
+      -Dphoto.dir.override="file://$(pwd)/../peer/src/test/resources/Photos" \
+      com.googlecode.fspotcloud.peer.Main   
