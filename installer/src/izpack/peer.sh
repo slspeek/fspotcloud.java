cf97b87e82bd02f1d334eddb238e1e9e0b59657d
Steven L. Speek <slspeek@gmail.com>
2012-04-06 22:23:11 +0200

    gradle builds war-prod and installer

--- installer/src/izpack/peer.sh
+++ installer/src/izpack/peer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Dpause=30 -Dendpoint=${application.id}.appspot.com -Dbot.secret=$bot.secret.userinput -Ddb=$photos.db.userinput  -jar $INSTALL_PATH/peer-jar-with-dependencies.jar
+java -Dpause=30 -Dendpoint=${application.id}.appspot.com -Dbot.secret=$bot.secret.userinput -Ddb=$photos.db.userinput  -cp $INSTALL_PATH/peer-jar-with-dependencies.jar com.googlecode.fspotcloud.peer.Main
