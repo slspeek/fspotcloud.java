72c80bbcdfd884a80284c7d0f2f3491ac8a08cbd
Steven L. Speek <slspeek@gmail.com>
2012-08-22 22:43:08 +0200

    Added one unit test 2

--- server/src/test/java/com/googlecode/fspotcloud/server/main/handler/GetUserGroupHandlerTest.java
+++ server/src/test/java/com/googlecode/fspotcloud/server/main/handler/GetUserGroupHandlerTest.java
@@ -3,8 +3,7 @@ package com.googlecode.fspotcloud.server.main.handler;
 import com.googlecode.fspotcloud.model.jpa.usergroup.UserGroupEntity;
 import com.googlecode.fspotcloud.server.model.api.UserGroup;
 import com.googlecode.fspotcloud.server.model.api.UserGroupDao;
-import com.googlecode.fspotcloud.shared.main.SaveUserGroupAction;
-import com.googlecode.fspotcloud.shared.main.UserGroupInfo;
+import com.googlecode.fspotcloud.shared.main.GetUserGroupAction;
 import com.googlecode.fspotcloud.user.UserService;
 import net.customware.gwt.dispatch.shared.DispatchException;
 import org.jukito.JukitoRunner;
@@ -13,24 +12,23 @@ import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
 
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 @RunWith(JukitoRunner.class)
-public class SaveUserGroupHandlerTest {
-
+public class GetUserGroupHandlerTest {
     public static final String FOO = "FOO";
     public static final String BAR = "Bar";
-    public static final String CLOSE_FAMILY = "Close family";
-    public static final String FAMILY = "Family";
-    @Inject private SaveUserGroupHandler handler;
-    @Inject private UserService userService;
-    @Inject private UserGroupDao userGroupDao;
+    public static final long ID = 1L;
+    @Inject
+    private GetUserGroupHandler handler;
+    @Inject
+    private UserService userService;
+    @Inject
+    private UserGroupDao userGroupDao;
 
     @Test(expected = UserIsNotLoggedException.class)
     public void testUnauthorized() throws Exception {
-
-        handler.execute(new SaveUserGroupAction(new UserGroupInfo(1L, FAMILY, CLOSE_FAMILY, false)), null);
+        handler.execute(new GetUserGroupAction(ID), null);
         verify(userService).isUserLoggedIn();
         verifyNoMoreInteractions(userGroupDao, userService);
     }
@@ -39,7 +37,7 @@ public class SaveUserGroupHandlerTest {
     public void testGroupDoesNotExist() throws Exception {
         when(userService.isUserLoggedIn()).thenReturn(true);
         when(userService.getEmail()).thenReturn("FOO");
-        handler.execute(new SaveUserGroupAction(new UserGroupInfo(1L, FAMILY, CLOSE_FAMILY, false)), null);
+        handler.execute(new GetUserGroupAction(ID), null);
         verify(userService).isUserLoggedIn();
         verify(userService).getEmail();
         verify(userGroupDao).find(1L);
@@ -53,7 +51,7 @@ public class SaveUserGroupHandlerTest {
         UserGroup userGroup = new UserGroupEntity();
         userGroup.setOwner(BAR);
         when(userGroupDao.find(1L)).thenReturn(userGroup);
-        handler.execute(new SaveUserGroupAction(new UserGroupInfo(1L, FAMILY, CLOSE_FAMILY, false)), null);
+        handler.execute(new GetUserGroupAction(ID), null);
         verify(userService).isUserLoggedIn();
         verify(userService).getEmail();
         verify(userGroupDao).find(1L);
@@ -67,14 +65,10 @@ public class SaveUserGroupHandlerTest {
         UserGroup userGroup = new UserGroupEntity();
         userGroup.setOwner(FOO);
         when(userGroupDao.find(1L)).thenReturn(userGroup);
-        handler.execute(new SaveUserGroupAction(new UserGroupInfo(1L, FAMILY, CLOSE_FAMILY, false)), null);
+        handler.execute(new GetUserGroupAction(ID), null);
         verify(userService).isUserLoggedIn();
         verify(userService).getEmail();
         verify(userGroupDao).find(1L);
-
-        verify(userGroupDao).save(userGroup);
-        assertEquals(userGroup.getDescription(), CLOSE_FAMILY);
-        assertEquals(userGroup.getName(), FAMILY);
         verifyNoMoreInteractions(userGroupDao, userService);
     }
 }
